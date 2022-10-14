db64147179cd99ad3948dc1b9ac81a663eb0aed7
Eilah974 <alex.acropolis@gmail.com>
2022-10-14 21:04:18 +0200

    db config modified

--- back/app/config/db.js
+++ back/app/config/db.js
@@ -12,9 +12,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
     pool: {
         max: 5,
-        min: 0,
+        min: 1,
         acquire: 30000,
-        idle: 60000,
+        idle: 10000,
     },
 
 });
